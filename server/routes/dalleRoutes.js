e0db4a5337b0edfd7ae6a316760527397d46228b
boosungkim <boosung@boosungkim.com>
2023-07-15 14:32:51 -0400

    added story generation api

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -18,7 +18,10 @@ router.route('/').get(async (req,res) => {
 
 router.route('/').post(async (req,res) => {
     try {
+        // Get the prompt
         const { prompt } = req.body;
+
+        // Generate the image using DALL-E
         const aiResponse = await openai.createImage({
             prompt,
             n:1,
@@ -27,6 +30,19 @@ router.route('/').post(async (req,res) => {
         });
         
         const image = aiResponse.data.data[0].b64_json;
+        
+        // Generate the short story using GPT
+        const storyResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {"role": "system", "content": "You are a short fictional story generator. Generate short stories based on the user prompt."},
+                {role: "user", content: prompt}
+            ],
+        });
+
+        const story = storyResponse.data.choices[0].message.content;
+
+        // Return the image and text
         res.status(200).json({ photo: image });
 
     } catch (error){
