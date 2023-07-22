3961dc12a21aeb22ae6bdd39639252ae7af1e4dd
boosungkim <boosung@boosungkim.com>
2023-07-22 15:23:38 +0900

    housecleaning stuff. added nav bar components and changed some descriptions on home page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,9 +14,14 @@ const App = () => {
         <img src={logo} alt="logo" className="w-16 object-contain" />
       </div>
       </Link>
-      <Link to="/create-story" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
-        Create
-      </Link>
+      <div className="flex items-center">
+        <Link to="https://boosungkim.com" className="font-inter font-medium bg-green-500 text-white px-4 py-2 rounded-md mr-4">
+          Personal Website
+        </Link>
+        <Link to="/create-story" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">
+          Create
+        </Link>
+      </div>
     </header>
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
--- client/src/page/StoryPage.jsx
+++ client/src/page/StoryPage.jsx
@@ -34,7 +34,7 @@ const StoryPage = () => {
     <div className="max-w-3xl mx-auto p-4">
       {story && (
         <div className="bg-white rounded-lg shadow-md p-6">
-          <h2 className="text-3xl font-semibold mb-4">Prompt: {story.prompt}</h2>
+          <h2 className="text-3xl font-semibold mb-4">{story.prompt}</h2>
           <p className="text-lg mb-4">User: {story.name}</p>
           <img className="w-full h-auto object-cover rounded-xl mb-4" src={story.photo} alt={story.prompt} />
           {story.story.map((paragraph, index) => (
